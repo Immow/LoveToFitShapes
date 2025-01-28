b2fd1ee3013d399d4d19d5f59383d14a829a2b27
Immow <k.schippers@gmail.com>
2025-01-28 17:34:27 +0100

    JapserFix

--- states/game.lua
+++ states/game.lua
@@ -27,6 +27,23 @@ local Object1 = newObject.new({
 	image = Assets[1]
 })
 
+local function getCellPosition(index, width)
+	return index % width + 1, math.floor(index / width + 1)
+end
+
+local function getNeighbours(index, width, height)
+	local x, y = getCellPosition(index - 1, width)
+
+	local neighbours = {}
+
+	if x > 0 then table.insert(neighbours, index - 1) end
+	if x < width - 1 then table.insert(neighbours, index + 1) end
+	if y > 0 then table.insert(neighbours, index - width) end
+	if y < height - 1 then table.insert(neighbours, index + width) end
+
+	return neighbours
+end
+
 local function getEmptyNeighbours(index)
 	local cells = {}
 
@@ -34,7 +51,9 @@ local function getEmptyNeighbours(index)
 	if index % gridWidth ~= 0 then cells[2] = index + 1 end
 	if index > gridWidth then cells[3] = index - gridWidth end
 	if index <= gridWidth * gridHeight - gridWidth then cells[4] = index + gridWidth end
-
+	for key, value in pairs(cells) do
+		print(value)
+	end
 	return cells
 end
 
@@ -43,37 +62,42 @@ local function drawGrid()
 		local xCord = offsetX + (((i % gridWidth) * cellSize))
 		local yCord = offsetY + math.floor((i) / gridHeight) * cellSize
 		love.graphics.rectangle("line", xCord, yCord, cellSize, cellSize)
+		love.graphics.setColor(0, 1, 0, 1)
 		love.graphics.print(Grid[i + 1], xCord, yCord)
+		love.graphics.setColor(1, 1, 1, 1)
 	end
 end
 
 local currentIndex = math.ceil((gridWidth * gridHeight) / 2)
 local shapeId = 1
+Grid[currentIndex] = shapeId
 local function floodFill()
-	local steps = love.math.random(1, 4)
-	if currentIndex == 0 then
-		currentIndex = shapeId
+	-- local steps = love.math.random(1, 4)
+	-- for i = 1, steps do
+	-- 	local cells = getEmptyNeighbours(currentIndex)
+	-- 	local pickedCell = love.math.random(1, #cells)
+	-- 	Grid[pickedCell] = shapeId
+	-- 	currentIndex = pickedCell
+	-- end
+	-- shapeId = shapeId + 1
+	-- local cells = getEmptyNeighbours(currentIndex)
+	-- for _, value in pairs(cells) do
+	-- 	Grid[value] = 1
+	-- end
+	local test = getNeighbours(1, gridWidth, gridHeight)
+	for key, value in pairs(test) do
+		Grid[value] = "cow"
 	end
-	for i = 1, steps do
-		local cells = getEmptyNeighbours(currentIndex)
-		local pickedCell = love.math.random(1, #cells)
-		Grid[pickedCell] = shapeId
-		currentIndex = pickedCell
-	end
-	shapeId = shapeId + 1
 end
 
 function Game:load()
 	genGrid()
 	-- local test = getEmptyNeighbours(10)
 	-- print(Tprint(test))
-	-- floodFill()
+	floodFill()
 
-	Grid[1] = 1
-	Grid[8] = 2
-
-	print(Tprint(Grid))
-	print("Game module loaded")
+	-- print(Tprint(Grid))
+	-- print("Game module loaded")
 end
 
 function Game:mousepressed(mx, my, mouseButton)
