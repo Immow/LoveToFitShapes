3672c875e4fe5fcd6b6ca3d14638965d74bb63d7
Immow <k.schippers@gmail.com>
2025-02-07 20:46:45 +0100

    bla

--- class/object.lua
+++ class/object.lua
@@ -33,21 +33,45 @@ function Object:update(dt)
 
 end
 
+local function calculateShapeSize(anchorPoints)
+	local maxX, maxY = 0, 0
+
+	for _, anchor in ipairs(anchorPoints) do
+		maxX = math.max(maxX, anchor.x * 50)
+		maxY = math.max(maxY, anchor.y * 50)
+	end
+
+	return maxX, maxY
+end
+
 function Object:draw()
 	love.graphics.print(self.rotation)
 	love.graphics.setColor(1, 1, 1, 1)
-	local ox, oy = self.image:getWidth() / 2, self.image:getHeight() / 2
-	love.graphics.draw(self.image, self.x, self.y, -(self.rotation - 1) * (math.pi / 2), 1, 1)
+	local ox, oy = calculateShapeSize(self.anchorPoints[self.rotation])
+	ox, oy = ox / 2, oy / 2
+
+	love.graphics.translate(self.x + ox, self.y + oy)
+	-- self.rotation = self.rotationIndex * (math.pi / 2)
+	love.graphics.rotate(-(self.rotation - 1) * (math.pi / 2))
+	love.graphics.translate(-ox, -oy)
+
+	love.graphics.draw(self.image)
+
+	love.graphics.origin()
+	love.graphics.translate(self.x, self.y)
+
+	love.graphics.rectangle("line", 0, 0, ox * 2, oy * 2)
+
 	love.graphics.setColor(1, 0, 0, 1)
 	-- love.graphics.circle("fill", self.x + 10, self.y, 5)
 	for _, anchor in ipairs(self.anchorPoints[self.rotation]) do
-		love.graphics.circle("fill", self.x + 50 / 2 + ((anchor.x - 1) * 50), self.y + 50 / 2 + ((anchor.y - 1) * 50), 5)
+		love.graphics.circle("fill", 50 / 2 + ((anchor.x - 1) * 50), 50 / 2 + ((anchor.y - 1) * 50), 5)
 		-- for _, coord in ipairs(anchor) do
 		-- 	-- print(coord.x)
 		-- end
 	end
 	love.graphics.setColor(0, 0, 1, 1)
-	love.graphics.circle("fill", self.x, self.y, 5)
+	love.graphics.circle("fill", 0, 0, 5)
 end
 
 return Object
